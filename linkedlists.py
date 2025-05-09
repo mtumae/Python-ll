beafaeef2d8b21d3860f3aa86c712628a41449dd
mtume <mtume2016@gmail.com>
2025-05-09 09:00:42 +0300

    initial commit

---
+++ linkedlists.py
@@ -0,0 +1,18 @@
+import llist
+
+class node:
+    def __init__(self, data):
+        self.data=data
+        self.next=None
+
+
+    def insert_beginning(self, data):
+        newNode = node(data)
+        if self.head == None:
+            self.head==newNode
+            return
+        else:
+            newNode.next = self.head
+            self.head=newNode
+
+
