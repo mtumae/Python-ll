b180c827a5969ae63127135876609dbfdcbd9750
mtume <mtume2016@gmail.com>
2025-05-09 10:04:55 +0300

    functions

--- linkedlists.py
+++ linkedlists.py
@@ -1,17 +1,30 @@
 
 class node:
     def __init__(self, data):
-        self.data=data
-        self.next=None
+        self.data = data
+        self.next = None
 
 
-    def insert_beginning(self, data):
-        newNode = node(data)
-        if self.head is None:
-            self.head==newNode
-            return
-        else:
-            newNode.next = self.head
-            self.head=newNode
+def insertbeg(head, data):
+    new_node = node(data)
+    new_node.next = head
+    return new_node
+
+
+
+def deletebeg(head):
+    if head is None:
+        print("List is empty")
+        return None
+
+    del(head)
+
+
+def traverse(head):
+    current = head
+    while current:
+        print(current.data, end=" -> ")
+        current = current.next
+    print("None")
 
 
